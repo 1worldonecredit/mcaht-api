b700394c54338860035d3169f8093fc08538d13e
1worldonecredit <168613138+1worldonecredit@users.noreply.github.com>
2026-09-05 09:53:40 +0700

    เพิ่ม limit ให้รองรับไฟล์รูปภาพขนาดใหญ่ (ใส่ไว้บรรทัดบนๆ ของ server.js)

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.use(cors({
 
 
 // เพิ่ม limit ให้รองรับไฟล์รูปภาพขนาดใหญ่ (ใส่ไว้บรรทัดบนๆ ของ server.js)
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 
 // ---------------------------------------------------------
